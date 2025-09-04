62372584d05f4c6ce37d1943757a8685cf624d02
ZainabImadulla <zainab.imadulla@icloud.com>
2025-09-03 21:45:45 -0400

    very messy start of typeORM config

---
+++ backend/src/database/factories/user.factory.ts
@@ -0,0 +1,12 @@
+import { setSeederFactory } from 'typeorm-extension';
+import { User } from '../../entities/User';
+
+export default setSeederFactory(User, (faker) => {
+    const user = new User();
+    user.id = faker.string.uuid();
+    user.firstName = faker.person.firstName()
+    user.lastName = faker.person.lastName();
+    user.email = faker.internet.email({firstName: user.firstName, lastName: user.lastName})
+
+    return user;
+})
