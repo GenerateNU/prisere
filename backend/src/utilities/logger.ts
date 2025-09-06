da798e452559c7eeac3024f20742d430f5a22102
jtoncelli <jack.toncelli@gmail.com>
2025-09-06 00:07:34 -0400

    comments

--- backend/src/utilities/logger.ts
+++ backend/src/utilities/logger.ts
@@ -9,10 +9,13 @@ if (!existsSync(LOG_DIR)) {
     mkdirSync(LOG_DIR, { recursive: true });
 }
 
+// remove all ansi character used for making the string look pretty in the console
+// they will just mess up the text in the log
 const stripAnsi = (str: string): string => {
     return str.replace(/\x1b\[[0-9;]*m/g, '').replace(/\x1b\[[0-9;]*[A-Za-z]/g, '');
 };
 
+// Implementation of a PrintFunc (https://hono.dev/docs/middleware/builtin/logger#printfunc)
 export const logMessageToFile = (message: string, ...rest: string[]) => {
     const timestamp = new Date().toISOString();
     const cleanMessage = stripAnsi(message);
@@ -24,6 +27,7 @@ export const logMessageToFile = (message: string, ...rest: string[]) => {
     });
 };
 
+// more generalized logger
 export const logObjectToFile = (data: object) => {
     const timestamp = new Date().toISOString();
     const fullMessage = `[${timestamp}] ${data}\n`;
