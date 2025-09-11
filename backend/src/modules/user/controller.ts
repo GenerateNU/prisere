543929d851a3fae1f187e4ca97bbcb3246e80561
Jack <115236356+jacktoncelli@users.noreply.github.com>
2025-09-11 19:13:11 -0400

    Openapi doc generation (#13)
    * v1 of openAPI docs, very messy
    
    * refactoring openapi routes to be a bit cleaner
    
    * removing unnecessary package and fixing compose to ignore logging file on watch
    
    * fix test to expect response code of 201
    
    * changing to use zod shcemas for payload validation
    
    * changing error handling
    
    * linter error :P

--- backend/src/modules/user/controller.ts
+++ backend/src/modules/user/controller.ts
@@ -1,8 +1,6 @@
 import { Context, TypedResponse } from "hono";
 import { IUserService } from "./service";
-import { CreateUserDTO, CreateUserAPIResponse } from "../../types/User";
-import { plainToInstance } from 'class-transformer';
-import { validateOrReject } from 'class-validator';
+import { CreateUserAPIResponse, CreateUserDTOSchema } from "../../types/User";
 import { withControllerErrorHandling } from "../../utilities/error";
 
 
@@ -19,9 +17,8 @@ export class UserController implements IUserController{
 
     createUser = withControllerErrorHandling(async (ctx: Context): Promise<TypedResponse<CreateUserAPIResponse>> => {
         const json = await ctx.req.json();
-        const payload = plainToInstance(CreateUserDTO, json);
-        await validateOrReject(payload);
+        const payload = CreateUserDTOSchema.parse(json)
         const user = await this.userService.createUser(payload);
-        return ctx.json(user, 200);
+        return ctx.json(user, 201);
     });
 }
