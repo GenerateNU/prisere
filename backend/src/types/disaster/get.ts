42049d765cd0ebd683c1287b5ba6770efaab2275
Manuel Torres <torres.man@northeastern.edu>
2025-09-18 18:23:23 -0400

    fixed getResponseSchema added get tests

--- backend/src/types/disaster/get.ts
+++ backend/src/types/disaster/get.ts
@@ -4,13 +4,13 @@ import { ErrorResponseSchema } from "../Utils";
 
 export const GetAllDisastersResponseSchema = z.array(
     z.object({
-        id: z.uuid(),
+        femaId: z.uuid(),
         disasterNumber: z.number(),
         state: FIPSState,
         declarationDate: z.iso.datetime(),
         startDate: z.iso.datetime().optional(),
         endDate: z.iso.datetime().optional(),
-        fipsCountyCodes: FIPSCounty,
+        fipsCountyCode: FIPSCounty,
         declarationType: z.string(),
         designatedArea: z.string(),
         designatedIncidentTypes: incidentTypeString,
