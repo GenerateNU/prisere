909a48dbea780136f78226aea9638a677c2ea8b9
ZainabImadulla <zainab.imadulla@icloud.com>
2025-09-19 23:32:20 -0400

    fix open-api pt.2

--- backend/src/modules/openapi/all-routes.ts
+++ backend/src/modules/openapi/all-routes.ts
@@ -11,11 +11,10 @@ export const setUpOpenApiRoutes = (db: DataSource) => {
         info: {
             version: "1.0.0",
             title: "Prisere API",
-        },
-        servers: [{ url: "/api" }],
+        }
     });
 
-    openApiApp.get("/docs", swaggerUI({ url: "/openapi/spec.json" }));
+    openApiApp.get("/docs", swaggerUI({ url: "api/openapi/spec.json" }));
     return openApiApp;
 };
 
