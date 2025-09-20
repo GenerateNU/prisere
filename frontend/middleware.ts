d32477e3d5f359a79990918442572ee39c5d6237
ZainabImadulla <zainab.imadulla@icloud.com>
2025-09-19 22:23:33 -0400

    added docker file

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,6 +1,6 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/utils/supabase/middleware'
-import { createClient } from './utils/supabase/server'
+import { createClient } from '@/utils/supabase/server'
 import { NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
