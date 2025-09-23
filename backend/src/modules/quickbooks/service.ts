3a36c55a4f8c648e1f87f61e25dafb26a88d59d3
Thomas Young <thomasjonyoung@gmail.com>
2025-09-23 17:45:57 -0400

    oh man

--- backend/src/modules/quickbooks/service.ts
+++ backend/src/modules/quickbooks/service.ts
@@ -3,11 +3,14 @@ import { QuickbooksClient } from "../../external/quickbooks/client";
 import { withServiceErrorHandling } from "../../utilities/error";
 import { IQuickbooksTransaction } from "./transaction";
 import { QuickbooksSession } from "../../entities/QuickbookSession";
+import Boom from "@hapi/boom";
+import { QBQueryResponse } from "../../external/quickbooks/types";
 
 export interface IQuickbooksService {
     generateAuthUrl(args: { userId: string }): Promise<string>;
     createQuickbooksSession(args: { code: string; state: string; realmId: string }): Promise<QuickbooksSession>;
     refreshQuickbooksSession(args: { refreshToken: string; companyId: string }): Promise<QuickbooksSession>;
+    consumeOAuthState(args: { state: string }): Promise<void>;
 }
 
 export class QuickbooksService implements IQuickbooksService {
@@ -56,8 +59,7 @@ export class QuickbooksService implements IQuickbooksService {
             const maybeToken = await this.transaction.fetchOAuth({ stateId: state });
 
             if (maybeToken?.stateId !== state) {
-                // TODO: error
-                throw new Error();
+                throw Boom.internal("State mismatch");
             }
 
             const { access_token, expires_in, x_refresh_token_expires_in, refresh_token } =
@@ -70,10 +72,8 @@ export class QuickbooksService implements IQuickbooksService {
             let companyId = external?.companyId;
 
             if (!external) {
-                console.log({ companyId: maybeToken.initiatorUser?.companyId });
                 if (!maybeToken.initiatorUser.companyId) {
-                    // TODO: better errror
-                    throw new Error("no company");
+                    throw Boom.badRequest("The requesting user deos not belong to a company");
                 }
 
                 await this.transaction.createCompanyRealm({
@@ -111,4 +111,82 @@ export class QuickbooksService implements IQuickbooksService {
             });
         }
     );
+
+    consumeOAuthState = withServiceErrorHandling(async ({ state }: { state: string }) => {
+        await this.transaction.clearPendingOAuth({ stateId: state });
+    });
+
+    private async retryClientApi<T>(
+        request: ClientRequest<T>,
+        {
+            existingSession,
+            externalId,
+            maxAttempts = 3,
+        }: { existingSession: QuickbooksSession; externalId: string; maxAttempts?: number }
+    ) {
+        let tries = 0;
+        let activeSession = existingSession;
+
+        while (tries < maxAttempts) {
+            tries++;
+            const result = await request({ session: activeSession, externalId });
+
+            if (result._id === "revoked") {
+                await this.transaction.destroyQuickbooksSession({ externalId });
+
+                throw Boom.unauthorized("Quickbooks session deleted");
+            }
+
+            if (result._id === "unauthorized") {
+                activeSession = await this.refreshQuickbooksSession({
+                    refreshToken: activeSession.refreshToken,
+                    companyId: activeSession.companyId,
+                });
+                continue;
+            }
+
+            return result.data;
+        }
+
+        throw Boom.internal("Quickbooks could not be reached");
+    }
+
+    private async makeRequestToQB<T>({ userId, request }: { userId: string; request: ClientRequest<T> }) {
+        const sessionInfo = await this.transaction.getSessionForUser({ userId });
+
+        let session = sessionInfo.session;
+        const externalId = sessionInfo.externalId;
+
+        const now = dayjs();
+
+        if (!session || !externalId || now.isSameOrAfter(session.refreshExpiryTimestamp)) {
+            throw Boom.unauthorized("Quickbooks session is expired");
+        }
+
+        if (now.isSameOrAfter(session.accessExpiryTimestamp)) {
+            session = await this.refreshQuickbooksSession({
+                refreshToken: session.refreshToken,
+                companyId: session.companyId,
+            });
+        }
+
+        return await this.retryClientApi(request, { existingSession: session, externalId });
+    }
+
+    // [FUTURE]: We can use this general pattern to query data from QB api w/ safe retries + session revalidation
+    _queryExample = withServiceErrorHandling(async ({ userId }: { userId: string }) => {
+        const data = this.makeRequestToQB({
+            userId,
+            request: async (session) => {
+                return await this.qbClient._exampleQueryData({
+                    qbRealm: session.externalId,
+                    accessToken: session.session.accessToken,
+                });
+            },
+        });
+
+        return data;
+    });
 }
+
+type ClientRequest<T> = (input: { session: QuickbooksSession; externalId: string }) => Promise<QBQueryResponse<T>>;
