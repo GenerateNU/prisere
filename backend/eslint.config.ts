bec528d936a6ec76139303322f72d538043373ac
Thomas Young <thomasjonyoung@gmail.com>
2025-09-24 11:46:04 -0400

    fixes

--- backend/eslint.config.ts
+++ backend/eslint.config.ts
@@ -12,7 +12,7 @@ export default defineConfig([
             parserOptions: {
                 ecmaVersion: "latest",
                 sourceType: "module",
-                project: "../tsconfig.json",
+                project: "./tsconfig.json",
             },
             globals: {
                 console: "readonly",
@@ -45,6 +45,7 @@ export default defineConfig([
             "no-var": "error",
             eqeqeq: ["error", "always"],
             curly: ["error", "all"],
+            "no-undef": "off", // not needed, we have TS to tell us what's undefined
         },
     },
     {
