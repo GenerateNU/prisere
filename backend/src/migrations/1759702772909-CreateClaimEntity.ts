5e07bc1dc8de9e4fa9f2d36596efaa877b94571b
Jack <115236356+jacktoncelli@users.noreply.github.com>
2025-10-05 18:29:28 -0400

    fixing one more time :sob: (#66)

--- backend/src/migrations/1759702772909-CreateClaimEntity.ts
+++ backend/src/migrations/1759702772909-CreateClaimEntity.ts
@@ -6,9 +6,6 @@ export class CreateClaimEntity1759702772909 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TYPE "public"."claim_status_enum" AS ENUM('ACTIVE', 'FILED')`);
         await queryRunner.query(`CREATE TABLE "claim" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "status" "public"."claim_status_enum" NOT NULL, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "companyId" uuid NOT NULL, "disasterId" character varying NOT NULL, CONSTRAINT "UQ_e15a58941d97f6bf90b4b69433c" UNIQUE ("companyId", "disasterId"), CONSTRAINT "PK_466b305cc2e591047fa1ce58f81" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_86586021a26d1180b0968f98502"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_86586021a26d1180b0968f98502" UNIQUE ("companyId")`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_86586021a26d1180b0968f98502" FOREIGN KEY ("companyId") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "claim" ADD CONSTRAINT "FK_70b2e2d5da91bd7206e15676bf1" FOREIGN KEY ("companyId") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "claim" ADD CONSTRAINT "FK_febac1e7fc933e9195b5f535e36" FOREIGN KEY ("disasterId") REFERENCES "fema_disaster"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
@@ -16,9 +13,6 @@ export class CreateClaimEntity1759702772909 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "claim" DROP CONSTRAINT "FK_febac1e7fc933e9195b5f535e36"`);
         await queryRunner.query(`ALTER TABLE "claim" DROP CONSTRAINT "FK_70b2e2d5da91bd7206e15676bf1"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "FK_86586021a26d1180b0968f98502"`);
-        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_86586021a26d1180b0968f98502"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "FK_86586021a26d1180b0968f98502" FOREIGN KEY ("companyId") REFERENCES "company"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`DROP TABLE "claim"`);
         await queryRunner.query(`DROP TYPE "public"."claim_status_enum"`);
     }
