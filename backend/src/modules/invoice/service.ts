3cff13ab6982071fd81e1a3967aa1f82877e77ec
Jack <115236356+jacktoncelli@users.noreply.github.com>
2025-10-05 16:29:12 -0400

    Create invoices entity and endpoints (#55)
    * round 1 invoice implementation
    
    * invoice routing and entities done. testing and openAPI todo
    
    * openapi routes done
    
    * cleanups, bug fixes, and tests
    
    * other migration, finished tests
    
    * test fix and linter
    
    * linter again
    
    * cleaning up tests and adding 404 routes
    
    * linting

---
+++ backend/src/modules/invoice/service.ts
@@ -0,0 +1,67 @@
+import { IInvoiceTransaction } from "./transaction";
+import { withServiceErrorHandling } from "../../utilities/error";
+import { CreateOrUpdateInvoicesDTO, GetCompanyInvoicesDTO } from "../../types/Invoice";
+import { Invoice } from "../../entities/Invoice";
+import Boom from "@hapi/boom";
+import { ICompanyTransaction } from "../company/transaction";
+import { validate } from "uuid";
+
+export interface IInvoiceService {
+    bulkCreateOrUpdateInvoice(payload: CreateOrUpdateInvoicesDTO): Promise<Invoice[]>;
+    getInvoiceById(id: string): Promise<Invoice>;
+    getInvoicesForCompany(payload: GetCompanyInvoicesDTO): Promise<Invoice[]>;
+}
+
+export class InvoiceService implements IInvoiceService {
+    private invoiceTransaction: IInvoiceTransaction;
+    private companyTransaction: ICompanyTransaction;
+
+    constructor(invoiceTransaction: IInvoiceTransaction, companyTransaction: ICompanyTransaction) {
+        this.invoiceTransaction = invoiceTransaction;
+        this.companyTransaction = companyTransaction;
+    }
+
+    bulkCreateOrUpdateInvoice = withServiceErrorHandling(
+        async (payload: CreateOrUpdateInvoicesDTO): Promise<Invoice[]> => {
+            const uniqueCompanyIds = [...new Set(payload.map((inv) => inv.companyId))];
+
+            // validate all uuids as being properly formatted
+            const badIds = uniqueCompanyIds.filter((id) => !validate(id));
+
+            if (badIds.length) {
+                throw Boom.badRequest(`Invalid uuid format: ${badIds.join(", ")}`);
+            }
+
+            // make sure all those companies actually exist in the DB to get a decent error message
+            const missingIds = await this.companyTransaction.validateCompaniesExist(uniqueCompanyIds);
+
+            if (missingIds.length !== 0) {
+                throw Boom.badRequest(`Companies not found: ${missingIds.join(", ")}`);
+            }
+
+            const newInvoices = await this.invoiceTransaction.createOrUpdateInvoices(payload);
+
+            if (!newInvoices || newInvoices.length === 0) {
+                throw Boom.internal("Failed to create invoices");
+            }
+
+            return newInvoices;
+        }
+    );
+
+    getInvoiceById = withServiceErrorHandling(async (id: string): Promise<Invoice> => {
+        const invoice = await this.invoiceTransaction.getInvoiceById(id);
+
+        if (!invoice) {
+            throw Boom.notFound("Invoice not found");
+        }
+
+        return invoice;
+    });
+
+    getInvoicesForCompany = withServiceErrorHandling(async (payload: GetCompanyInvoicesDTO): Promise<Invoice[]> => {
+        const invoices = await this.invoiceTransaction.getInvoicesForCompany(payload);
+
+        return invoices;
+    });
+}
---
+++ backend/src/tests/invoices/getById.test.ts
@@ -0,0 +1,59 @@
+import { Hono } from "hono";
+import { describe, test, expect, beforeAll, beforeEach, afterEach } from "bun:test";
+import { startTestApp } from "../setup-tests";
+import { IBackup } from "pg-mem";
+import { SeederFactoryManager } from "typeorm-extension";
+import CompanySeeder from "../../database/seeds/company.seed";
+import { InvoiceSeeder, seededInvoices } from "../../database/seeds/invoice.seed";
+import { DataSource } from "typeorm";
+import { CompareRequestToCreated } from "./utils";
+
+describe("Invoice get by id", () => {
+    let app: Hono;
+    let backup: IBackup;
+    let datasource: DataSource;
+    const seededInvoice = seededInvoices[0];
+    const seededInvoiceId = seededInvoice.id;
+
+    beforeAll(async () => {
+        const testAppData = await startTestApp();
+        app = testAppData.app;
+        backup = testAppData.backup;
+        datasource = testAppData.dataSource;
+    });
+
+    beforeEach(async () => {
+        const companySeeder = new CompanySeeder();
+        await companySeeder.run(datasource, {} as SeederFactoryManager);
+
+        const invoiceSeeder = new InvoiceSeeder();
+        await invoiceSeeder.run(datasource, {} as SeederFactoryManager);
+    });
+
+    afterEach(async () => {
+        backup.restore();
+    });
+
+    test("GET /quickbooks/invoices/:id - id that exists", async () => {
+        const response = await app.request(`/quickbooks/invoice/${seededInvoiceId}`);
+        expect(response.status).toBe(200);
+        const body = await response.json();
+        CompareRequestToCreated([seededInvoice], [body]);
+    });
+
+    test("GET /quickbooks/invoices/:id - id doesn't exist", async () => {
+        const response = await app.request(`/quickbooks/invoice/8d720d89-d047-4f19-a999-1934f914908d`); // bad uuid
+        expect(response.status).toBe(404);
+        const body = await response.json();
+        expect(body).toHaveProperty("error");
+        expect(body.error).toBe("No invoices found with id: 8d720d89-d047-4f19-a999-1934f914908d");
+    });
+
+    test("GET /quickbooks/invoices/:id - invalid UUID", async () => {
+        const response = await app.request(`/quickbooks/invoice/8d720d89-134f914908d`); // invalid uuid
+        expect(response.status).toBe(400);
+        const body = await response.json();
+        expect(body).toHaveProperty("error");
+        expect(body.error).toBe("Invalid invoice ID format");
+    });
+});
