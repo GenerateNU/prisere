86e08545519bb4b375dd67daf16b25ff0c02ff56
ZainabImadulla <zainab.imadulla@icloud.com>
2025-10-05 16:03:06 -0400

    added healthcheck routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,4 @@
-import { Hono } from "hono";
+import { Context, Hono, TypedResponse } from "hono";
 import { DataSource } from "typeorm";
 import { userRoutes } from "./modules/user/route";
 import { setUpOpenApiRoutes } from "./modules/openapi/all-routes";
@@ -6,8 +6,10 @@ import { locationAddressRoute } from "./modules/location-address/route";
 import { companyRoutes } from "./modules/company/route";
 import { disasterRoutes } from "./modules/disaster/route";
 import { disasterNotificationRoutes } from "./modules/disasterNotifications/route";
+import { StatusCode } from "hono/utils/http-status";
 
 export const setUpRoutes = (app: Hono, db: DataSource) => {
+    app.route("/", healthRoutes())
     app.route("/users", userRoutes(db));
     app.route("/location-address", locationAddressRoute(db));
     app.route("/companies", companyRoutes(db));
@@ -15,3 +17,15 @@ export const setUpRoutes = (app: Hono, db: DataSource) => {
     app.route("/disaster", disasterRoutes(db));
     app.route("/disasterNotification", disasterNotificationRoutes(db));
 };
+
+
+const healthRoutes = (): Hono => {
+    const app = new Hono();
+    app.get("/", (ctx: Context): TypedResponse<{message:string}, StatusCode> => {
+        return ctx.json({ message: "Welcome to Prisere" }, 200);
+    } )
+    app.get("/healthcheck", (ctx: Context): TypedResponse<{message:string}, StatusCode>=> {
+        return ctx.json({ message: "OK" }, 200);
+      });
+    return app
+}
