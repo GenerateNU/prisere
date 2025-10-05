b7579f21a6641760a7aa17df87428006494b1c95
Thomas Young <thomasjonyoung@gmail.com>
2025-10-05 17:14:31 -0400

    feat(quickbooks): quickbooks oauth integration (#47)
    * basic session creation implemented
    
    * oh man
    
    * fixes
    
    * add to prettier ignore
    
    * add csrf
    
    * add updated at/created at to tables
    
    * add docs, address comments
    
    * fix relation
    
    * tests!
    
    * open api
    
    * change config back?
    
    * add tags

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -6,6 +6,7 @@ import { locationAddressRoute } from "./modules/location-address/route";
 import { companyRoutes } from "./modules/company/route";
 import { disasterRoutes } from "./modules/disaster/route";
 import { disasterNotificationRoutes } from "./modules/disasterNotifications/route";
+import { quickbooksRoutes } from "./modules/quickbooks/routes";
 import { invoiceRoutes } from "./modules/invoice/route";
 
 export const setUpRoutes = (app: Hono, db: DataSource) => {
@@ -15,5 +16,6 @@ export const setUpRoutes = (app: Hono, db: DataSource) => {
     app.route("/openapi", setUpOpenApiRoutes(db));
     app.route("/disaster", disasterRoutes(db));
     app.route("/disasterNotification", disasterNotificationRoutes(db));
+    app.route("/quickbooks", quickbooksRoutes(db));
     app.route("/quickbooks/invoice", invoiceRoutes(db));
 };
