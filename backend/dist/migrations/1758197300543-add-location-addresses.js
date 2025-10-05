99bbded8240e258923842939ec85f7f829705982
Kyra Holmes <kyra@holmesmail.com>
2025-10-04 23:40:38 -0400

    Remove dist folder from git tracking

--- backend/dist/migrations/1758197300543-add-location-addresses.js
+++
@@ -1,9 +0,0 @@
-export class AddLocationAddresses1758197300543 {
-    name = "AddLocationAddresses1758197300543";
-    async up(queryRunner) {
-        await queryRunner.query(`CREATE TABLE "location_address" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "country" character varying NOT NULL, "stateProvince" character varying NOT NULL, "city" character varying NOT NULL, "streetAddress" character varying NOT NULL, "postalCode" integer NOT NULL, "county" character varying, CONSTRAINT "PK_bf1188fd425a5c4f19d6fa22c2e" PRIMARY KEY ("id"))`);
-    }
-    async down(queryRunner) {
-        await queryRunner.query(`DROP TABLE "location_address"`);
-    }
-}
--- backend/dist/types/Utils.js
+++
@@ -1,4 +0,0 @@
-import { z } from "zod";
-export const ErrorResponseSchema = z.object({
-    error: z.string(),
-});
