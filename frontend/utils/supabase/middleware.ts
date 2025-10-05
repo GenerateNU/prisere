9f16a64f4fd4f2ad1be1726708526504db2ea3ec
ZainabImadulla <zainab.imadulla@icloud.com>
2025-10-05 18:46:06 -0400

    actually fixed middleware

--- frontend/utils/supabase/middleware.ts
+++ frontend/utils/supabase/middleware.ts
@@ -28,5 +28,19 @@ export async function updateSession(request: NextRequest) {
       },
     }
   )
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth') &&
+    !request.nextUrl.pathname.startsWith('/error')
+  ) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  }
   return supabaseResponse
 }
