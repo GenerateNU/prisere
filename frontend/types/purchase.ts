d9a063019912420ee04b02ba4c2a11f6546d72e2
Manuel Torres <torres.man@northeastern.edu>
2025-10-12 20:14:03 -0400

    intial

--- frontend/types/purchase.ts
+++ frontend/types/purchase.ts
@@ -1,4 +1,3 @@
 import type { paths } from "../schema";
 
-// Now you can extract types like this:
 export type Purchase = paths["/purchase/{id}"]["get"]["responses"]["200"]["content"]["application/json"];
