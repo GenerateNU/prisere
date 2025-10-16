e67d012f731039944619dcdd96b0ed501c24c2b0
Kyra Holmes <147226101+kyraholmes@users.noreply.github.com>
2025-10-15 20:27:28 -0400

    Skeleton dashboard page (#88)
    * everything done but endpoints not tested
    
    * removed extra things on query key

---
+++ frontend/api/purchase.ts
@@ -0,0 +1,27 @@
+import { authHeader, authWrapper, client } from "./client";
+
+export const sumPurchasesByCompanyAndDateRange = async (
+    startDate: Date,
+    endDate: Date,
+): Promise<{ total: number }> => {
+    const req = async (token: string): Promise<{ total: number }> => {
+        const { data, error, response } = await client.GET("/purchase/bulk/totalExpenses", {
+            headers: authHeader(token),
+            params: {
+                query: {
+                    startDate: startDate.toISOString(),
+                    endDate: endDate.toISOString(),
+                },
+            }
+        });
+
+        if (response.ok) {
+            return data!;
+        } else {
+            throw Error(error?.error);
+        }
+    }
+
+    return authWrapper<{ total: number }>()(req);
+}
+
---
+++ frontend/types/purchase.ts
@@ -0,0 +1,2 @@
+import type { paths } from "../schema";
+export type Purchase = paths["/purchase/bulk/totalExpenses"]["get"]["responses"]["200"]["content"]["application/json"];
