0af372965f88c7f2bf14017a099a0ab209a527f3
jtoncelli <jack.toncelli@gmail.com>
2025-10-20 16:12:22 -0400

    trying server side

--- frontend/api/client.ts
+++ frontend/api/client.ts
@@ -7,9 +7,6 @@ const apiBaseRoute =
         ? process.env.PROD_API_BASE_URL
         : process.env.LOCAL_API_BASE_URL;
 
-console.log('API Base Route:', apiBaseRoute);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-
 export const client = createClient<paths>({ baseUrl: apiBaseRoute });
 
 export const authHeader = (token: string, contentType: string = "application/json") => {
--- frontend/api/user.ts
+++ frontend/api/user.ts
@@ -1,7 +1,9 @@
+'use server';
 import { CreateUserRequest, User } from "@/types/user";
 import { authHeader, authWrapper, client } from "./client";
 
 export const createUser = async (payload: CreateUserRequest): Promise<User> => {
+    console.log("Create user:", process.env.PROD_API_BASE_URL);
     const req = async (token: string): Promise<User> => {
         const { data, error, response } = await client.POST("/users", {
             headers: authHeader(token),
