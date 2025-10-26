72bfe74bb69f7cba3438674a1fc4ab8ab20e547f
jtoncelli <jack.toncelli@gmail.com>
2025-10-26 13:14:21 -0400

    fixing notification migrations

--- backend/src/migrations/1759072454465-DisasterNotifications.ts
+++ backend/src/migrations/1759072454465-DisasterNotifications.ts
@@ -10,7 +10,7 @@ export class DisasterNotifications1759072454465 implements MigrationInterface {
         await queryRunner.query(
             `CREATE TYPE "public"."disasterNotification_notificationstatus_enum" AS ENUM('unread', 'read')`
         );
-        await queryRunner.query(`CREATE TABLE "notifications" (
+        await queryRunner.query(`CREATE TABLE "disasterNotification" (
             "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
             "userId" uuid NOT NULL,
             "femaDisasterId" character varying NOT NULL,
@@ -22,17 +22,17 @@ export class DisasterNotifications1759072454465 implements MigrationInterface {
             CONSTRAINT "PK_e910e9a1629b878751458edf731" PRIMARY KEY ("id")
         )`);
         await queryRunner.query(
-            `ALTER TABLE "notifications" ADD CONSTRAINT "FK_520e2b43d1ce6828e112c82927e" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+            `ALTER TABLE "disasterNotification" ADD CONSTRAINT "FK_520e2b43d1ce6828e112c82927e" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
         );
         await queryRunner.query(
-            `ALTER TABLE "notifications" ADD CONSTRAINT "FK_4313c838cd09ff0a0f599b986a8" FOREIGN KEY ("femaDisasterId") REFERENCES "fema_disaster"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+            `ALTER TABLE "disasterNotification" ADD CONSTRAINT "FK_4313c838cd09ff0a0f599b986a8" FOREIGN KEY ("femaDisasterId") REFERENCES "fema_disaster"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
         );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "notifications" DROP CONSTRAINT "FK_4313c838cd09ff0a0f599b986a8"`);
-        await queryRunner.query(`ALTER TABLE "notifications" DROP CONSTRAINT "FK_520e2b43d1ce6828e112c82927e"`);
-        await queryRunner.query(`DROP TABLE "notifications"`);
+        await queryRunner.query(`ALTER TABLE "disasterNotification" DROP CONSTRAINT "FK_4313c838cd09ff0a0f599b986a8"`);
+        await queryRunner.query(`ALTER TABLE "disasterNotification" DROP CONSTRAINT "FK_520e2b43d1ce6828e112c82927e"`);
+        await queryRunner.query(`DROP TABLE "disasterNotification"`);
         await queryRunner.query(`DROP TYPE "public"."disasterNotification_notificationstatus_enum"`);
         await queryRunner.query(`DROP TYPE "public"."disasterNotification_notificationtype_enum"`);
     }
