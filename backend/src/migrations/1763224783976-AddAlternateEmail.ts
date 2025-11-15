53313e0d37cd89fa65e5b81bd4a9de25610c795d
jtoncelli <jack.toncelli@gmail.com>
2025-11-15 18:56:11 -0500

    frontend updated and backend is fixed

--- backend/src/migrations/1763224783976-AddAlternateEmail.ts
+++ backend/src/migrations/1763224783976-AddAlternateEmail.ts
@@ -4,11 +4,11 @@ export class AddAlternateEmail1763224783976 implements MigrationInterface {
     name = 'AddAlternateEmail1763224783976'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "alternateEmail" character varying`);
+        await queryRunner.query(`ALTER TABLE "company" ADD "alternateEmail" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "alternateEmail"`);
+        await queryRunner.query(`ALTER TABLE "company" DROP COLUMN "alternateEmail"`);
     }
 
 }
