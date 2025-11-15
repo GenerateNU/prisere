bde778c0e37ffea51eca4935f4b12379123abaa9
Jack <115236356+jacktoncelli@users.noreply.github.com>
2025-11-15 18:27:55 -0500

    adding purchase line item enum types be/fe (#166)

--- frontend/app/expense-tracker/expense-table/disaster-options.tsx
+++ frontend/app/expense-tracker/expense-table/disaster-options.tsx
@@ -1,7 +1,7 @@
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Command, CommandGroup, CommandItem } from "@/components/ui/command";
-import { DisasterType } from "@/types/purchase";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { DISASTER_TYPE_COLORS, DISASTER_TYPE_LABELS, DISASTER_TYPE_LABELS_TO_CHANGE } from "@/types/disaster";
+import { DisasterType } from "@/types/purchase";
 
 interface DisasterLabelProps {
     disasterType: DisasterType;
