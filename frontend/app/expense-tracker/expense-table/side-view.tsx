9c093f94daaaaf321b14eb036bf4585fdf90edbd
ZainabImadulla <zainab.imadulla@icloud.com>
2025-12-04 21:24:48 -0500

    fixed button

--- frontend/app/expense-tracker/expense-table/side-view.tsx
+++ frontend/app/expense-tracker/expense-table/side-view.tsx
@@ -17,23 +17,25 @@ export default function ExpenseSideView({ purchase, open, onOpenChange }: SideVi
     if (!purchase) return null;
 
     return (
-        <Sheet open={open} onOpenChange={onOpenChange}>
+        <Sheet open={open} onOpenChange={onOpenChange} >
             <SheetContent className="w-full sm:max-w-3xl overflow-y-auto">
-                <SheetHeader className="pt-13 px-6">
-                    <SheetTitle className="text-2xl">{purchase.vendor ? purchase.vendor : "Unknown Vendor"}</SheetTitle>
-                </SheetHeader>
+                <div className="px-6 pt-10">
+                    <SheetHeader className="pt-13 px-5">
+                        <SheetTitle className="text-2xl">{purchase.vendor ? purchase.vendor : "Unknown Vendor"}</SheetTitle>
+                    </SheetHeader>
 
-                <div className="space-y-4 mb-8">
-                    <DetailRow label="Amount" value={"$" + (purchase.totalAmountCents / 100).toFixed(2)} />
-                    <DetailRow
-                        label="Category"
-                        value={categoryTagsMainPurchase(getCategoriesString(purchase.lineItems))}
-                    />
-                    <DetailRow label="Date" value={new Date(purchase.dateCreated).toLocaleDateString()} />
-                    <DetailRow
-                        label="Disaster Related"
-                        value={<DisasterTypeTag type={getPurchaseTypeString(purchase.lineItems)} />}
-                    />
+                    <div className="space-y-4 mb-8 ">
+                        <DetailRow label="Amount" value={"$" + (purchase.totalAmountCents / 100).toFixed(2)} />
+                        <DetailRow
+                            label="Category"
+                            value={categoryTagsMainPurchase(getCategoriesString(purchase.lineItems))}
+                        />
+                        <DetailRow label="Date" value={new Date(purchase.dateCreated).toLocaleDateString()} />
+                        <DetailRow
+                            label="Disaster Related"
+                            value={<DisasterTypeTag type={getPurchaseTypeString(purchase.lineItems)} />}
+                        />
+                    </div>
                 </div>
 
                 <div className="border-t pt-8 mx-[5%]">
